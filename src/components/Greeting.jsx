da8a22e53d2a667d7f815bb6605e0997fb333cf1
camo <camilo.solarte@claro.com.co>
2023-02-25 22:10:07 -0500

    Empezamos con el manejo de props

---
+++ src/components/Greeting.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+function Greeting({message}) {
+  // const user = {
+  //   name: "Camilo",
+  //   lastName: "Solarte",
+  // };
+  // function add (x, y) {
+  //   return x + y;
+  // }
+  return (
+    <>
+      <div>This component says: {message}</div>
+    </>
+  );
+}
+
+export default Greeting;
